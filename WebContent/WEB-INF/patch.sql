289d4b93b9a816f8e337dd9ac17e12b946a158c2
Aviskar Basnet <aviskarbasnet@yahoo.com>
2019-02-06 10:41:00 +0545

    Patch created.

---
+++ WebContent/WEB-INF/patch.sql
@@ -0,0 +1,24 @@
+﻿create database sunway-servlet-app;
+
+use sunway-servlet-app;
+
+CREATE TABLE `student` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `age` tinyint(4) NOT NULL,
+  `gender` enum('Male','Female') CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `address` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `phoneno` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `user` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+INSERT INTO `sunway-servlet-app`.`user`
+(username, password)
+VALUES('aviskar', 'pass');
